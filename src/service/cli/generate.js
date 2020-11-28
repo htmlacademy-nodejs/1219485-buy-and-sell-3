e6c88007ea24df7de7d96e434ec1d893e22fcfbb
Vyacheslav <vyachesl.n@gmail.com>
2020-11-28 19:27:50 +0600

    Подключает файл с константами в generate

---
+++ src/service/cli/generate.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const {
+  DEFAULT_COUNT,
+  FILE_NAME,
+  TITLES,
+  SENTENCES,
+  CATEGORIES,
+  OfferType,
+  SumRestrict,
+  PictureRestrict
+} = require(`./const`);
+
+module.exports = {
+  name: `--generate`,
+  run() {
+  }
+}
